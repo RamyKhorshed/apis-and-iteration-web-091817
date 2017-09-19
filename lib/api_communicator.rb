1d1aeb31f121d3f6022cc3e07b6e772a2775e3a1
Ramy Khorshed <RamyKhorshed@gmail.com>
2017-09-19 13:33:20 -0400

    Added helper functions

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,35 +4,8 @@ require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
-  output = []
-  filmArray = []
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  results = character_hash["results"]
-    results.each do |person|
-      if person["name"].downcase == character
-        output = person["films"]
-      end
-    end
-    output.each do |link|
-      moviedata = RestClient.get(link)
-      moviedata_hash = JSON.parse(moviedata)
-      filmArray << moviedata_hash
-    end
-  #  if results[:name] == "character"
-  #    puts results[:films]
-  #  end
-
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
-  return filmArray
+  movie_links = character_movies(character)
+  movie_info(movie_links)
 end
 
 
@@ -56,3 +29,26 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+def character_movies(character)
+  output = []
+  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  character_hash = JSON.parse(all_characters)
+  results = character_hash["results"]
+  results.each do |person|
+    if person["name"].downcase == character
+      output = person["films"]
+    end
+  end
+  return output
+end
+
+def movie_info(output)
+  filmArray = []
+  output.each do |link|
+    moviedata = RestClient.get(link)
+    moviedata_hash = JSON.parse(moviedata)
+    filmArray << moviedata_hash
+  end
+  return filmArray
+end
